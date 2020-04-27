de5db1e9af9cdf6705236a7e5997569b81540da2
Carlos Castro García <alu0100819847@ull.edu.es>
2020-04-27 18:44:03 +0200

    Changing style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
