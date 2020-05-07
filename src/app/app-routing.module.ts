47bb0b3d74667a345aaf211d695bdaf21cf21cb9
Carlos Castro García <alu0100819847@ull.edu.es>
2020-05-07 20:47:27 +0200

    Adding description and complete menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {InfoComponent} from './info/info.component';
 import {MainComponent} from './main/main.component';
 import {DescriptionComponent} from './description/description.component';
+import {ContactComponent} from './contact/contact.component';
 
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'index', component: InfoComponent},
-  {path: 'description', component: DescriptionComponent}
+  {path: 'description', component: DescriptionComponent},
+  {path: 'contact', component: ContactComponent}
   ];
 
 @NgModule({
