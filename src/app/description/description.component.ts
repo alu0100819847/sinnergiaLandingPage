0cc5dd5ed1e358080a440288c83c80076d909367
Carlos Castro García <alu0100819847@ull.edu.es>
2020-08-31 14:37:22 +0100

    First beta

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-description',
+  selector: 'app-description-sinnergia',
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.css']
 })
